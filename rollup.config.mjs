35bf678009cb9366b17f9768d013ce240d46085b
raduh <radu.hulubas@gmail.com>
2022-12-30 20:45:14 +0200

    chore: add web components support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,13 +2,29 @@
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss'
 
 export default [
   // ES Modules
   {
+    input: 'src/dzs-chip-selector/dzs-chip-selector--web-components.ts',
+    output: {
+      file: 'dist/dzsChipSelectorWebComponents.js', format: 'es',
+    },
+    plugins: [
+      typescript(),
+      babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
+      terser(),
+      postcss({
+        plugins: []
+      })
+    ],
+  },
+  // ES Modules
+  {
     input: 'src/dzs-chip-selector/dzs-chip-selector.ts',
     output: {
-      file: 'dist/index.es.js', format: 'es',
+      file: 'dist/index.js', format: 'es',
     },
     plugins: [
       typescript(),
