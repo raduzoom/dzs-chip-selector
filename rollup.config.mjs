860ecdd3d72dc6c8f3cc70df718cd0252d06d3a2
raduh <radu.hulubas@gmail.com>
2023-01-22 22:10:45 +0200

    fix: add bootstrap to demo pages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -24,7 +24,7 @@ export default [
       copy({
           targets: [
             { src: 'src/dzs-chip-selector/style/skins', dest: 'dist/style' },
-            { src: ['src/dzs-chip-selector/dzs-chip-selector.css', 'src/style/table-for-jsDoc.css'], dest: 'dist/style' },
+            { src: ['src/dzs-chip-selector/dzs-chip-selector.css', 'src/style/table-for-jsDoc.css', 'src/style/bootstrap.min.css'], dest: 'dist/style' },
       ]}),
       terser(),
     ],
